86a41f59e07ca32ac6232a5e9db8aae5ed725275
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-02-24 14:38:08 -0700

    created all routes in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Recipes } from './recipes/recipes';
+import { Meals } from './meals/meals';
+import { Pages } from './pages/pages';
+import { Profile} from './Profile/Profile';
+import { CreateMeal } from './createmeal/createmeal';
+import { CreatePage } from './createpage/createpage';
+import { CreatePost } from './createpost/createpost';
+import { CreateRecipe } from './createrecipe/createrecipe';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <BrowserRouter>
+  <header>
+        <nav>
+          <menu>
+            <div className="nav-item"><NavLink className="nav-link" to="home">Home</NavLink></div>
+            <div className="nav-item"><NavLink className="nav-link" to="recipes">Recipes</NavLink></div>
+            <div className="nav-item"><NavLink className="nav-link" to="meals">Find-A-Meal</NavLink></div>
+            <div className="nav-item"><NavLink className="nav-link" to="pages">Pages</NavLink></div>
+            <div className="nav-item"><NavLink className="nav-link" to="profile">Profile</NavLink></div>
+            <div className="nav-item"><NavLink className="nav-link" to="">LogOut</NavLink></div>
+          </menu>
+
+        <div>
+          <form method="get" action="search.html">
+            <span>search</span>
+            <input type="text" placeholder="search" />
+            <button type="submit" class="btn btn-primary">Search</button>
+            </form>
+        </div>
+
+        </nav>
+      </header>
+      <Routes>
+      <Route path='/home' element={<Home />}/>
+      <Route path='/recipes' element={<Recipes />} />
+      <Route path='/meals' element={<Meals />} />
+      <Route path='/pages' element={<Pages />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/login' element={Login}   exact />
+      <Route path='/createmeal' element={<CreateMeal />} />
+      <Route path='/createpage' element={<CreatePage />} />
+      <Route path='/createpost' element={<CreatePost />} />
+      <Route path='/createrecipe' element={<CreateRecipe />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+      <footer>
+      <span className="text-reset">Porter Nichols</span>
+      <a href="https://github.com/DaBigMoose1101/startup">GitHub</a>
+  </footer>    
+  </BrowserRouter>
+  )
+  function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
+      
 }
