3b39ee77de096e139a3e0f494147c33ada8825b4
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-03-31 14:49:18 -0600

    intialized all necessary variables and end points

--- service/index.js
+++ service/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+let users = [];
+let posts = [];
+
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
